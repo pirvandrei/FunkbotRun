b654c875d1df5b0cfe57222d3dcfab3e7c8cc9b9
Andrei Pirvan <pirvandrei@gmail.com>
2018-03-21 10:59:49 +0100

    12RBS - AddedRemoteCarClient&CarServer to steer remotely

--- src/SyncMotors/TestingSynconizedMotorsMain.java
+++ src/SyncMotors/TestingSynconizedMotorsMain.java
@@ -7,16 +7,30 @@ import lejos.hardware.motor.*;
 import lejos.hardware.port.MotorPort;
 
 public class TestingSynconizedMotorsMain {
-
+	
+	static Arbitrator arby;
 	public static void main(String[] args) {
+		
+		System.out.println("Program is starting...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 		RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 		Behavior b1 = new BehaviourForward(leftMotor,rightMotor);
 		InfraredAdapter ir = new InfraredAdapter();
-		Behavior b2 = new BehaviourAvoidObject(leftMotor,rightMotor,ir);
+	    Behavior b2 = new BehaviourAvoidObject(leftMotor,rightMotor,ir);
 		Behavior b3 = new BehaviourStopByTouch();
 		Behavior[] b1b2b3 = {b1,b2,b3};
-		Arbitrator arby = new Arbitrator(b1b2b3);
+		 
+		 
+		
+		
+		arby = new Arbitrator(b1b2b3);
+		
 		arby.go();
 	}
 	
